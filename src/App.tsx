d7443db9b654ca332286f514af468117860651e0
jimmyphangit <jimmyphan1@hotmail.com>
2025-08-29 00:43:59 -0400

    Implemented dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import Header from './layout/Header'
 import { About, Work, Projects, Contact } from './components/sections'
 
+
 function App() {
   return (
-    <>
+    <div className="min-h-screen bg-[#18181b] text-white">
       <Header />
       <About />
       <Work />
       <Projects />
       <Contact />
-    </>
+    </div>
   )
 }
 
