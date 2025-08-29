a1aacdf105e54182ece9faab85c604b89524030d
jimmyphangit <jimmyphan1@hotmail.com>
2025-08-29 00:58:58 -0400

    Added framer motion

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import Header from './layout/Header'
 import { About, Work, Projects, Contact } from './components/sections'
-
+import SplashScreen from './components/splash-screen/SplashScreen'
 
 function App() {
   return (
-    <div className="min-h-screen bg-[#18181b] text-white">
+    <div className="min-h-screen bg-[#18181b] text-white relative">
+      {/* Splash screen overlays everything initially */}
+      <SplashScreen />
+
       <Header />
       <About />
       <Work />
