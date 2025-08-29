e68259512fd63cdd0d2171ee2197c6eaea2f1041
jimmyphangit <jimmyphan1@hotmail.com>
2025-08-28 22:08:01 -0400

    Implemented sticky header

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import './App.css'
+import Header from './components/Header'
 
 function App() {
   return (
     <>
+      <Header />
       <section className="full-section section-1">
         <h1>Section 1</h1>
       </section>
