d8248c3437eb4a1e1f52aad991e948e908b5216c
jimmyphangit <jimmyphan1@hotmail.com>
2025-08-29 02:03:03 -0400

    Fixed fade so things load in time

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,31 @@
+import { useState, useEffect } from "react"
 import Header from './layout/Header'
 import { About, Work, Projects, Contact } from './components/sections'
 import SplashScreen from './components/splash-screen/SplashScreen'
 
 function App() {
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const timer = setTimeout(() => setLoading(false), 1800)
+    return () => clearTimeout(timer)
+  }, [])
+
   return (
     <div className="min-h-screen bg-[#18181b] text-white relative">
-      {/* Splash screen overlays everything initially */}
-      <SplashScreen />
+      {/* Splash screen always visible while loading */}
+      {loading && <SplashScreen />}
 
-      <Header />
-      <About />
-      <Work />
-      <Projects />
-      <Contact />
+      {/* Entire site hidden until loading is done */}
+      {!loading && (
+        <div className="transition-opacity duration-500 opacity-100">
+          <Header />
+          <About />
+          <Work />
+          <Projects />
+          <Contact />
+        </div>
+      )}
     </div>
   )
 }
